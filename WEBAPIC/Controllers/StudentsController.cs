1419fffa108065aabaa658f9fc5f02b3e0e8d240
htorrico <htorrico@truenorthcorporation.com>
2025-11-17 18:48:23 -0500

    Students Controllers

---
+++ WEBAPIC/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPIC.Models;
+
+namespace WEBAPIC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {            
+            var students = _context.Students.ToList();         
+            return students;
+        }
+
+        [HttpPost]
+        public void Insert(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+        }
+    }
+}
